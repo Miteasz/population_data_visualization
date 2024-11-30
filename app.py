43b0a0a18aa6a73c4278122f3e0dc112720b807e
Miteasz <bartekszudra@gmail.com>
2024-11-30 13:50:19 +0100

    Dodano mape geo

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 # Importowanie potrzebnych bibliotek
+import folium
+import requests
 import pandas as pd
 import streamlit as st
 import altair as alt
+import seaborn as sns
+import geopandas as gpd
+import matplotlib.pyplot as plt
 
 # Wczytanie danych
 countries_and_continents_path = 'countries_and_continents.csv'
@@ -24,10 +29,17 @@ population_male = population_male[population_male['Country Name'].isin(valid_cou
 
 # Przygotowanie danych
 def prepare_data():
-    death_rate['Year'] = death_rate['Atrybut']
+    # Sprawdzenie i zmiana nazw kolumn w birth_rate i death_rate
+    if 'Country Name' in birth_rate.columns:
+        birth_rate.rename(columns={'Country Name': 'Country'}, inplace=True)
+    if 'Country Name' in death_rate.columns:
+        death_rate.rename(columns={'Country Name': 'Country'}, inplace=True)
+
+    death_rate['Year'] = death_rate['Atrybut']  # Zakładamy, że 'Atrybut' zawiera rok
+    birth_rate['Year'] = birth_rate['Atrybut']
+
     population_female['Year'] = population_female['Atrybut']
     population_male['Year'] = population_male['Atrybut']
-    birth_rate['Year'] = birth_rate['Atrybut']
 
     # Dodanie kolumny Gender
     population_female['Gender'] = 'Female'
@@ -35,15 +47,31 @@ def prepare_data():
 
     # Połączenie danych o populacji mężczyzn i kobiet
     population_data = pd.concat([population_female, population_male]).reset_index(drop=True)
+
+    # Łączenie danych z kontynentami
     population_data = population_data.merge(countries_and_continents, 
                                             left_on='Country Name', 
                                             right_on='Country', 
                                             how='inner')
-    # Tworzenie kolumny Continent_Gender dla kolorowania
-    population_data['Continent_Gender'] = population_data['Continent'] + '-' + population_data['Gender']
-    return population_data
+
+    # Dodanie danych o narodzinach i zgonach
+    birth_rate.rename(columns={'Wartość': 'Birth Rate'}, inplace=True)
+    death_rate.rename(columns={'Wartość': 'Death Rate'}, inplace=True)
+
+    # Scalanie narodzin i zgonów z danymi populacyjnymi
+    combined_data = population_data.merge(birth_rate[['Country', 'Year', 'Birth Rate']], 
+                                          on=['Country', 'Year'], how='left')
+    combined_data = combined_data.merge(death_rate[['Country', 'Year', 'Death Rate']], 
+                                        on=['Country', 'Year'], how='left')
+
+    # Dodanie kolumny Continent_Gender
+    combined_data['Continent_Gender'] = combined_data['Continent'] + '-' + combined_data['Gender']
+
+    return combined_data
+
 
 prepared_data = prepare_data()
+# prepared_data.to_csv("prepared_data.csv")
 
 # Wizualizacje
 st.title("Wizualizacja danych demograficznych")
@@ -130,3 +158,77 @@ bar_chart = alt.Chart(top_countries_data).mark_bar().encode(
 
 if not top_countries_data.empty:
     st.altair_chart(bar_chart)
+
+#############
+
+# Przygotowanie danych geoprzestrzennych
+def prepare_geospatial_data(data):
+    # Wczytanie pliku GeoJSON z internetu
+    geojson_path = 'https://raw.githubusercontent.com/johan/world.geo.json/master/countries.geo.json'
+    geojson_data = requests.get(geojson_path).json()
+
+    # Przekształcenie GeoJSON na GeoDataFrame
+    geo_gdf = gpd.GeoDataFrame.from_features(geojson_data["features"])
+
+    # Ustawienie CRS (układu współrzędnych)
+    geo_gdf.set_crs(epsg=4326, inplace=True)
+
+    # Grupowanie danych dla wskaźnika na poziomie krajowym
+    map_data = data.groupby('Country')['Death Rate'].mean().reset_index()
+
+    # Dopasowanie nazw krajów (dodaj więcej mapowań w razie potrzeby)
+    country_name_mapping = {
+        "United States": "United States of America",
+        "Serbia": "Republic of Serbia",
+        "Tanzania": "United Republic of Tanzania",
+        "Democratic Republic of the Congo": "Republic of the Congo"
+    }
+    map_data['Country'] = map_data['Country'].replace(country_name_mapping)
+
+    # Łączenie danych GeoJSON z danymi wskaźników
+    geo_gdf = geo_gdf.merge(map_data, left_on='name', right_on='Country', how='left')
+
+    # Wypełnianie brakujących wartości wskaźnika (opcjonalnie)
+    geo_gdf['Death Rate'] = geo_gdf['Death Rate'].fillna(0)  # Wartość domyślna dla brakujących danych
+
+    return geo_gdf
+
+# Przygotowanie danych do mapy
+geo_data = prepare_geospatial_data(prepared_data)
+# geo_data.to_csv("geo_data.csv")
+# Tworzenie mapy z folium
+def create_map(geo_data):
+    # Tworzenie mapy
+    m = folium.Map(location=[20, 0], zoom_start=2)
+
+    # Dodanie warstwy Choropleth
+    folium.Choropleth(
+        geo_data=geo_data.to_json(),
+        data=geo_data,
+        columns=['name', 'Death Rate'],
+        key_on='feature.properties.name',
+        fill_color='YlGnBu',
+        fill_opacity=0.7,
+        line_opacity=0.2,
+        legend_name='Death Rate'
+    ).add_to(m)
+
+    # Dodanie tooltipów z informacjami o krajach
+    folium.GeoJson(
+        geo_data,
+        tooltip=folium.GeoJsonTooltip(
+            fields=['name', 'Death Rate'],
+            aliases=['Country', 'Death Rate'],
+            localize=True
+        )
+    ).add_to(m)
+
+    return m
+
+# Tworzenie mapy
+st.subheader("Mapa geoprzestrzenna: Śmiertelność na poziomie krajowym")
+map_object = create_map(geo_data)
+
+# Wyświetlenie mapy w Streamlit
+st.components.v1.html(map_object._repr_html_(), width=800, height=600)
+
